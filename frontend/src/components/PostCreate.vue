9ec6e6b21c24be1f808550a98f27f300cd49f685
Berk Cambaz <berkcambaz12321@gmail.com>
2022-08-24 12:42:16 +0300

    Add send button & text counter to post create

--- frontend/src/components/PostCreate.vue
+++ frontend/src/components/PostCreate.vue
@@ -1,13 +1,18 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue';
+import SendIcon from './Icons/SendIcon.vue';
 
 const input = ref<HTMLInputElement | null>(null);
+const text = ref({
+  limit: 256,
+  current: 0
+}).value;
 
 const onInput = () => {
   const elem = input.value;
   if (!elem) return;
-  //if (elem.value.length > text.limit) elem.value = elem.value.substring(0, text.limit);
-  //text.current = elem.value.length;
+  if (elem.value.length > text.limit) elem.value = elem.value.substring(0, text.limit);
+  text.current = elem.value.length;
   elem.style.height = "0";
   elem.style.height = elem.scrollHeight + "px";
 }
@@ -18,12 +23,16 @@ onMounted(() => { onInput(); })
 <template>
   <div class="post-create">
     <textarea class="input" ref="input" placeholder="Write your thoughts..." @input="onInput"></textarea>
+    <div class="bottom">
+      <SendIcon class="icon" />
+      <span>{{ `${text.current}/${text.limit}` }}</span>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .post-create {
-  padding: 1.5rem 0 1rem;
+  padding: 1.5rem 0 0.5rem 0;
 }
 
 .input {
@@ -37,6 +46,15 @@ onMounted(() => { onInput(); })
   border-radius: 0;
 
   border-bottom: 1px solid #000000;
+}
+
+.bottom {
+  display: flex;
+  align-items: center;
+}
+
+.icon {
+  cursor: pointer;
 
 }
 </style>
