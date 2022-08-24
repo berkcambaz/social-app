3afd3bec19506a122d352f1051713f7ef65e1fc3
Berk Cambaz <berkcambaz12321@gmail.com>
2022-08-24 12:56:49 +0300

    Implement login & signup views

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -2,4 +2,56 @@
 </script>
 
 <template>
+  <div class="signup">
+    <input type="text" class="input" placeholder="usertag...">
+    <input type="password" class="input" placeholder="password...">
+    <button class="button">login</button>
+    <span class="text">i don't have an account</span>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.signup {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  >* {
+    margin: 0.5rem 0;
+  }
+}
+
+.input {
+  box-sizing: content-box;
+
+  border: 0;
+  outline: 0;
+  border-radius: 0;
+
+  border-bottom: 1px solid #000000;
+}
+
+.button {
+  cursor: pointer;
+
+  border: 0;
+  border-radius: 5px;
+
+  background-color: #000000;
+  color: #ffffff;
+
+  padding: 0.25rem 1rem;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.75);
+  }
+}
+
+.text {
+  cursor: pointer;
+
+  &:hover {
+    border-bottom: 1px solid #000000;
+  }
+}
+</style>
--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -2,4 +2,57 @@
 </script>
 
 <template>
+  <div class="signup">
+    <input type="text" class="input" placeholder="usertag...">
+    <input type="email" class="input" placeholder="email...">
+    <input type="password" class="input" placeholder="password...">
+    <button class="button">signup</button>
+    <span class="text">i already have an account</span>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.signup {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  >* {
+    margin: 0.5rem 0;
+  }
+}
+
+.input {
+  box-sizing: content-box;
+
+  border: 0;
+  outline: 0;
+  border-radius: 0;
+
+  border-bottom: 1px solid #000000;
+}
+
+.button {
+  cursor: pointer;
+
+  border: 0;
+  border-radius: 5px;
+
+  background-color: #000000;
+  color: #ffffff;
+
+  padding: 0.25rem 1rem;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.75);
+  }
+}
+
+.text {
+  cursor: pointer;
+
+  &:hover {
+    border-bottom: 1px solid #000000;
+  }
+}
+</style>
