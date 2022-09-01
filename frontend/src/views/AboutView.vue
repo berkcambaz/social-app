83f34c64eeda4fb1a02d29622fc39041e13747e4
Berk Cambaz <berkcambaz12321@gmail.com>
2022-09-01 13:31:52 +0300

    Change about page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -4,31 +4,7 @@
 <template>
   <div class="about">
     <div class="title">Chernolink</div>
-    <div class="text">
-      we value your privacy<br />
-      we do not track you<br />
-      no saved locations<br />
-      no tracking cookies<br />
-    </div>
-    <div class="text">
-      freedom of speech<br />
-      no cancels<br />
-      no bans<br />
-      no suspensions<br />
-    </div>
-    <div class="text">
-      you decide what to see<br />
-      no promotions<br />
-      no recommendations<br />
-      no shady algorithms<br />
-    </div>
-    <div class="text">
-      the all experience is yours<br />
-      and<br />
-      only<br />
-      yours<br />
-    </div>
-    <p id="copyright">&copy; 2022 Chernolink</p>
+    <div>&copy; 2022 Chernolink</div>
   </div>
 </template>
 
@@ -46,12 +22,6 @@
 
 .title {
   font-size: $font-big;
-  margin-top: 2rem;
-}
-
-.text {
-  font-size: $font-medium-big;
-  text-align: center;
 }
 </style>
   
