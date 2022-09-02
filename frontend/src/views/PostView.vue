8908ebf89267a3c6ebd0406d91a153ce4b201c31
Berk Cambaz <berkcambaz12321@gmail.com>
2022-09-02 19:38:47 +0300

    Revert "Update PostView.vue"
    This reverts commit 57e31ba3338797a408b25d1fb747263fef7e1184.

--- frontend/src/views/PostView.vue
+++ frontend/src/views/PostView.vue
@@ -11,8 +11,7 @@ const postId = parseInt(router.currentRoute.value.params.id as string);
 const posts = usePosts();
 const post = ref<IPost | null>(null);
 
-const showCreateComment = ref(false);
-const showCreateReply = ref(false);
+const show = ref(true);
 
 const onInit = async () => {
   await posts.fetchPost(postId);
@@ -24,15 +23,15 @@ const onBottom = async () => { }
 </script>
 
 <template>
-  <Post class="main-post" v-if="post" :post="post" />
+  <Post v-if="post" :post="post" />
   <LoaderContainer :onInit="onInit" :onTop="onTop" :onBottom="onBottom">
 
   </LoaderContainer>
+  <HoverMenu :show="show">
+
+  </HoverMenu>
 </template>
 
 <style lang="scss" scoped>
-.main-post {
-  border-bottom: 0;
-}
 </style>
   
