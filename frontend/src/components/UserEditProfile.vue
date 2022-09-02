b0e448e113c5f7e22242ae3cf14155c4bf20a9bd
Berk Cambaz <berkcambaz12321@gmail.com>
2022-09-02 17:05:32 +0300

    Use hover menu on user edit profile

--- frontend/src/components/UserEditProfile.vue
+++ frontend/src/components/UserEditProfile.vue
@@ -7,9 +7,10 @@ import Loader from './Loader.vue';
 import Input from './Input.vue';
 import Button from './Button.vue';
 import { i18n } from '@/util/i18n';
+import HoverMenu from './HoverMenu.vue';
 
 const { t } = i18n.global;
-const { editingProfile, user } = defineProps<{ editingProfile: boolean, user: IUser }>();
+const { show, user } = defineProps<{ show: boolean, user: IUser }>();
 const users = useUsers();
 const loader = createLoader();
 
@@ -45,8 +46,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="user-edit-profile" v-if="editingProfile || loader.status">
-    <div class="background" @click="editingProfile = false"></div>
+  <HoverMenu :show="show || loader.status">
     <div class="container">
       <div class="text-section">
         <label for="username">{{  t("username")  }}</label>
@@ -58,41 +58,16 @@ onMounted(() => {
         {{  `${bioText.current}/${bioText.limit}`  }}
       </div>
       <Input type="text" :text="bioText" id="bio" />
-      <Button @click="async () => { if (await done()) editingProfile = false }" :disabled="loader.status">done</Button>
+      <Button @click="async () => { if (await done()) show = false }" :disabled="loader.status">done</Button>
       <div v-if="loader.status" class="loader-container">
         <Loader />
       </div>
     </div>
-  </div>
+  </HoverMenu>
 </template>
 
 <style lang="scss" scoped>
-.user-edit-profile {
-  position: absolute;
-  margin-top: 1rem;
-}
-
-.background {
-  z-index: 1000;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-
-  background-color: rgba(0, 0, 0, 0.25);
-}
-
 .container {
-  z-index: 1001;
-  position: fixed;
-  left: 50%;
-  transform: translateX(-50%);
-
-  background-color: #ffffff;
-  border-radius: 5px;
-  padding: 1rem;
-
   display: flex;
   flex-direction: column;
 
--- frontend/src/views/PostView.vue
+++ frontend/src/views/PostView.vue
@@ -5,10 +5,13 @@ import { ref } from 'vue';
 import type { IPost } from '../../../shared/types';
 import Post from '../components/Post.vue';
 import LoaderContainer from '../components/LoaderContainer.vue';
+import HoverMenu from '../components/HoverMenu.vue';
 
 const postId = parseInt(router.currentRoute.value.params.id as string);
 const posts = usePosts();
 const post = ref<IPost | null>(null);
+
+const show = ref(true);
 
 const onInit = async () => {
   await posts.fetchPost(postId);
@@ -24,6 +27,9 @@ const onBottom = async () => { }
   <LoaderContainer :onInit="onInit" :onTop="onTop" :onBottom="onBottom">
 
   </LoaderContainer>
+  <HoverMenu :show="show">
+
+  </HoverMenu>
 </template>
 
 <style lang="scss" scoped>
