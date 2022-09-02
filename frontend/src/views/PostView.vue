46a9fdaff229d9540f4ce6af72ead00bd1af69fb
Berk Cambaz <berkcambaz12321@gmail.com>
2022-09-02 10:04:20 +0300

    Add post not found text & show connected posts

--- frontend/src/views/PostView.vue
+++ frontend/src/views/PostView.vue
@@ -11,16 +11,26 @@ import { i18n } from '@/util/i18n';
 
 const posts = usePosts();
 const { t } = i18n.global;
+const connectedPosts = ref<IPost[]>([]);
 const mainPost = ref<IPost | null>(null);
 const postId = parseInt(router.currentRoute.value.params["id"] as string);
 let mainPostLoading = ref(true);
 
 const onInit = async () => {
-  // TODO: Implement post not found
   await posts.fetchPost(postId);
   mainPost.value = posts.getPost(postId);
   mainPostLoading.value = false;
   if (mainPost.value === null) return;
+
+  let commentId = mainPost.value.commentId;
+  for (; commentId !== -1;) {
+    if (posts.getPost(commentId) === null) await posts.fetchPost(commentId);
+    const post = posts.getPost(commentId)
+    if (post === null) break;
+    connectedPosts.value.push(post);
+    commentId = post.commentId;
+  }
+
   await posts.fetchPostComments(mainPost.value.id, "newer", true);
 }
 
@@ -34,7 +44,10 @@ const onBottom = async () => {
 </script>
 
 <template>
-  <Post v-if="mainPost" :post="mainPost" class="main-post" />
+  <div class="post-wrapper">
+    <Post v-for="post in connectedPosts" :post="post" />
+    <Post v-if="mainPost" :post="mainPost" class="main-post" />
+  </div>
   <div v-if="!mainPost && !mainPostLoading" class="not-found">
     {{  t("post_not_found")  }}
   </div>
@@ -45,6 +58,12 @@ const onBottom = async () => {
 </template>
 
 <style lang="scss" scoped>
+.post-wrapper {
+  &>* {
+    border-bottom: 1px dashed #000000;
+  }
+}
+
 .main-post {
   border-bottom: 0;
 }
