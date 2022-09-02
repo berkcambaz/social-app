ec530be17b5645748756b1a78d3da703b5612ec8
Berk Cambaz <berkcambaz12321@gmail.com>
2022-09-02 20:19:40 +0300

    Build frontend

--- frontend/dist/assets/AccountView.659d119e.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./index.155a4c1b.js";const e={};function n(r,t){return null}const o=c(e,[["render",n]]);export{o as default};
---
+++ frontend/dist/assets/AccountView.f2046cf3.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./index.c2e8c3fe.js";const e={};function n(r,t){return null}const o=c(e,[["render",n]]);export{o as default};
