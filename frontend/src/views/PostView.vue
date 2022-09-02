22dcad1c328127cc001fff6d1c581a27d72e042b
Berk Cambaz <berkcambaz12321@gmail.com>
2022-09-02 09:16:02 +0300

    Show post not found & "comment_create_text" locale

--- frontend/src/views/PostView.vue
+++ frontend/src/views/PostView.vue
@@ -6,15 +6,20 @@ import { ref } from 'vue';
 import type { IPost } from '../../../shared/types';
 import { usePosts } from '@/stores/posts';
 import PostCreate from '../components/PostCreate.vue';
+import { createLoader } from '@/util/loader';
+import { i18n } from '@/util/i18n';
 
 const posts = usePosts();
+const { t } = i18n.global;
 const mainPost = ref<IPost | null>(null);
 const postId = parseInt(router.currentRoute.value.params["id"] as string);
+let mainPostLoading = ref(true);
 
 const onInit = async () => {
   // TODO: Implement post not found
   await posts.fetchPost(postId);
   mainPost.value = posts.getPost(postId);
+  mainPostLoading.value = false;
   if (mainPost.value === null) return;
   await posts.fetchPostComments(mainPost.value.id, "newer", true);
 }
@@ -30,6 +35,9 @@ const onBottom = async () => {
 
 <template>
   <Post v-if="mainPost" :post="mainPost" class="main-post" />
+  <div v-if="!mainPost && !mainPostLoading" class="not-found">
+    {{  t("post_not_found")  }}
+  </div>
   <PostCreate v-if="mainPost" :postId="mainPost.id" />
   <LoaderContainer :onInit="onInit" :onTop="onTop" :onBottom="onBottom">
     <Post v-if="mainPost" v-for="post in  posts.getPostComments(mainPost)" :post="post" :key="post.id" />
@@ -39,5 +47,9 @@ const onBottom = async () => {
 <style lang="scss" scoped>
 .main-post {
   border-bottom: 0;
+}
+
+.not-found {
+  padding: 1rem 0;
 }
 </style>
