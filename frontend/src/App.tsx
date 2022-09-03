5273200abdb80c937350d07cf17af1b100c7784a
Berk Cambaz <berkcambaz12321@gmail.com>
2022-09-03 16:32:01 +0300

    Add global styles and normalize.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+import { Normalize } from 'styled-normalize'
+
 import Router from "./routes/_Router";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    overflow-y: scroll;
+
+    font-size: 16px;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+
+    // Disable highlight on mobile's when clicking
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
 
 function App() {
   return (
-    <div>
+    <>
+      <Normalize />
+      <GlobalStyle />
       <Router />
-    </div>
+    </>
   )
 }
 
