a66d2e56fac8e4611083f71dbf68fb4c671ec98b
Berk Cambaz <berkcambaz12321@gmail.com>
2022-09-03 12:27:56 +0300

    Create hover menu component

---
+++ frontend/src/components/HoverMenu.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+const { show } = defineProps<{ show: boolean }>();
+</script>
+  
+<template>
+  <div class="hover-menu" v-if="show">
+    <div class="background" @click="show = false"></div>
+    <div class="container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+  
+<style lang="scss" scoped>
+.hover-menu {
+  position: absolute;
+  margin-top: 1rem;
+}
+
+.background {
+  z-index: 1000;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  background-color: rgba(0, 0, 0, 0.25);
+}
+
+.container {
+  z-index: 1001;
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+
+  background-color: #ffffff;
+  border-radius: 5px;
+  padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+
+  &>* {
+    margin-bottom: 1rem;
+  
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
--- frontend/src/components/UserEditProfile.vue
+++ frontend/src/components/UserEditProfile.vue
@@ -7,6 +7,7 @@ import Loader from './Loader.vue';
 import Input from './Input.vue';
 import Button from './Button.vue';
 import { i18n } from '@/util/i18n';
+import HoverMenu from './HoverMenu.vue';
 
 const { t } = i18n.global;
 const { editingProfile, user } = defineProps<{ editingProfile: boolean, user: IUser }>();
@@ -45,57 +46,26 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="user-edit-profile" v-if="editingProfile || loader.status">
-    <div class="background" @click="editingProfile = false"></div>
-    <div class="container">
-      <div class="text-section">
-        <label for="username">{{  t("username")  }}</label>
-        {{  `${usernameText.current}/${usernameText.limit}`  }}
-      </div>
-      <Input type="text" :text="usernameText" id="username" />
-      <div class="text-section">
-        <label for="bio">{{  t("bio")  }}</label>
-        {{  `${bioText.current}/${bioText.limit}`  }}
-      </div>
-      <Input type="text" :text="bioText" id="bio" />
-      <Button @click="async () => { if (await done()) editingProfile = false }" :disabled="loader.status">done</Button>
-      <div v-if="loader.status" class="loader-container">
-        <Loader />
-      </div>
+  <HoverMenu :show="editingProfile">
+    <div class="text-section">
+      <label for="username">{{  t("username")  }}</label>
+      {{  `${usernameText.current}/${usernameText.limit}`  }}
     </div>
-  </div>
+    <Input type="text" :text="usernameText" id="username" />
+    <div class="text-section">
+      <label for="bio">{{  t("bio")  }}</label>
+      {{  `${bioText.current}/${bioText.limit}`  }}
+    </div>
+    <Input type="text" :text="bioText" id="bio" />
+    <Button @click="async () => { if (await done()) editingProfile = false }" :disabled="loader.status">done</Button>
+    <div v-if="loader.status" class="loader-container">
+      <Loader />
+    </div>
+  </HoverMenu>
 </template>
 
 <style lang="scss" scoped>
-.user-edit-profile {
-  position: absolute;
-  margin-top: 1rem;
-}
-
-.background {
-  z-index: 1000;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-
-  background-color: rgba(0, 0, 0, 0.25);
-}
-
 .container {
-  z-index: 1001;
-  position: fixed;
-  left: 50%;
-  transform: translateX(-50%);
-
-  background-color: #ffffff;
-  border-radius: 5px;
-  padding: 1rem;
-
-  display: flex;
-  flex-direction: column;
-
   &>* {
     margin-bottom: 1rem;
 
