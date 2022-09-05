cb88c751df076fc404024e0f127e60c01f4f5e39
Berk Cambaz <berkcambaz12321@gmail.com>
2022-09-05 12:17:54 +0300

    Implement menu route

--- frontend/src/routes/Menu.tsx
+++ frontend/src/routes/Menu.tsx
@@ -1,8 +1,42 @@
+import { Bookmark, Info, Logout, Person, Translate } from "@styled-icons/material-rounded";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import { setRoute } from "../store/slices/appSlice";
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Item = styled.div`
+  cursor: pointer;
+  
+  display: flex;
+  align-items: center;
+  width: 100%;
+
+  border-bottom: 1px solid #000000;
+
+  &:last-child {
+    border-bottom: 0;
+  }
+`;
+
+const Icon = styled.button`
+  box-sizing: content-box;
+  width: 32px;
+  height: 32px;
+  padding: 0.75rem;
+`;
+
+const Text = styled.span`
+  font-size: ${props => props.theme.font.big}px;
+`;
+
 function Menu() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -12,7 +46,36 @@ function Menu() {
     }))
   }, [])
 
-  return <div>menu</div>
+  const gotoAccount = () => navigate("/account");
+  const gotoBookmarks = () => navigate("/bookmarks");
+  const gotoLanguages = () => navigate("/languages");
+  const gotoAbout = () => navigate("/about");
+  const logout = () => navigate("/login");
+
+  return (
+    <Wrapper>
+      <Item onClick={gotoAccount}>
+        <Icon as={Person} />
+        <Text>account</Text>
+      </Item>
+      <Item onClick={gotoBookmarks}>
+        <Icon as={Bookmark} />
+        <Text>bookmarks</Text>
+      </Item>
+      <Item onClick={gotoLanguages}>
+        <Icon as={Translate} />
+        <Text>languages</Text>
+      </Item>
+      <Item onClick={gotoAbout}>
+        <Icon as={Info} />
+        <Text>about</Text>
+      </Item>
+      <Item onClick={logout}>
+        <Icon as={Logout} />
+        <Text>log out</Text>
+      </Item>
+    </Wrapper>
+  )
 }
 
 export default Menu
