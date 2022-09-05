792b3170851616690cbe42506e12c814902778b8
Berk Cambaz <berkcambaz12321@gmail.com>
2022-09-05 12:06:09 +0300

    Implement search route

--- frontend/src/routes/Search.tsx
+++ frontend/src/routes/Search.tsx
@@ -1,7 +1,17 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import UserSummary from "../components/UserSummary";
+import SingleInput from "../components/Util/SingleInput";
 import { setRoute } from "../store/slices/appSlice";
+
+const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  margin: 1rem 0;
+`;
 
 function Search() {
   const dispatch = useDispatch();
@@ -14,7 +24,23 @@ function Search() {
     }))
   }, [])
 
-  return <div>search</div>
+  return (
+    <div>
+      <InputWrapper>
+        <SingleInput type="text" placeholder="user..." />
+      </InputWrapper>
+      <UserSummary />
+      <UserSummary />
+      <UserSummary />
+      <UserSummary />
+      <UserSummary />
+      <UserSummary />
+      <UserSummary />
+      <UserSummary />
+      <UserSummary />
+      <UserSummary />
+    </div>
+  )
 }
 
 export default Search
