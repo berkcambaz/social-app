d7006772b2279f06ea142bc1e07e5b68ae1d0c2e
Berk Cambaz <berkcambaz12321@gmail.com>
2022-09-11 19:19:46 +0300

    Build frontend after reverting buggy updates

--- frontend/dist/assets/AccountView.3b4ecadf.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./index.0db901d9.js";const e={};function n(r,t){return null}const o=c(e,[["render",n]]);export{o as default};
---
+++ frontend/dist/assets/AccountView.dc74c667.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./index.f7b1a3c4.js";const e={};function n(r,t){return null}const o=c(e,[["render",n]]);export{o as default};
