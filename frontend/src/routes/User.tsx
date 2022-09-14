ad9803c54b697397b480c656956d3444e1f59850
Berk Cambaz <berkcambaz12321@gmail.com>
2022-09-14 16:55:16 +0300

    Fix user posts being fetched twice on render

--- frontend/src/routes/User.tsx
+++ frontend/src/routes/User.tsx
@@ -39,6 +39,7 @@ function UserRoute() {
 
   useEffect(() => {
     (async () => {
+      if (showUser) return;
       if (user) await useWait(() => fetchUserPosts(user.id, "newer"))();
       setShowPosts(true);
     })()
